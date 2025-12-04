1ec75b3883e98e8522c4ef725048c67bcb72fb89
kihoon <nike6736@naver.com>
2025-12-04 16:53:47 +0900

    fix: url연결 수정

--- files/urls_html.py
+++ files/urls_html.py
@@ -4,7 +4,7 @@ from .views_html import file_upload_page, file_list_page, file_delete_page
 app_name = "html_files"
 
 urlpatterns = [
+    path("", file_list_page, name="file_list_page"),
     path("upload/", file_upload_page, name="file_upload_page"),
-    path("list/", file_list_page, name="file_list_page"),
     path("delete/<int:pk>/", file_delete_page, name="file_delete_page"),
 ]
